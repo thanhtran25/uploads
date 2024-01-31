add43785cc278391b43bfd7b944f6c4be6603c37
Thanh Tran <thuthanhtran2610@gmail.com>
2024-01-31 10:21:07 +0700

    init source

---
+++ src/shared/connections/database.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { DataSource } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import path from 'path';
+import { Config } from '../config/env.config';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: Config.DB_HOST,
+  port: Config.DB_PORT,
+  username: Config.DB_USER,
+  password: Config.DB_PASSWORD,
+  database: Config.DB_NAME,
+  entities: [path.join(__dirname, '../**/*.entity{.js,.ts}')],
+  migrations: [path.join(__dirname, '../migrations/*{.js,.ts}')],
+  namingStrategy: new SnakeNamingStrategy(),
+  logging: Config.LOGGING === 'true' ? true : false,
+});
