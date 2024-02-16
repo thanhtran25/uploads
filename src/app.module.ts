54bb52ac2be9d38b7f16ecfe3fd1f0b607a08f6a
Thanh Tran <thuthanhtran2610@gmail.com>
2024-02-16 11:59:43 +0700

    create entity and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { InventoriesModule } from './modules/inventories/inventories.module';
 import { ProductsModule } from './modules/products/products.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [UsersModule, InventoriesModule, ProductsModule],
+  imports: [UsersModule, InventoriesModule, ProductsModule, CategoriesModule],
 })
 export class AppModule {}
